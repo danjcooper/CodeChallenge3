1a2c197b12f62d36e4ac13d507c554cff9fa7070
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2021-08-10 15:51:45 +0100

    add Repo Component

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -1,11 +1,23 @@
 import React from "react";
+import "style.css";
 
-const Repo = () => {
+const Repo = ({ repoName, issues, forks, stars, commits }) => {
   return (
-    <>
-      <h1>Hello</h1>
-      <h2>I'm a repo</h2>
-    </>
+    <article className="RepoInfo">
+      <h2>{repoName}</h2>
+      <h3>
+        Stars: <span className="dataPoint">{stars}</span>
+      </h3>
+      <h3>
+        Commits: <span className="dataPoint">{commits}</span>
+      </h3>
+      <h3>
+        Forks: <span className="dataPoint">{forks}</span>
+      </h3>
+      <h3>
+        Issues: <span className="dataPoint">{issues}</span>
+      </h3>
+    </article>
   );
 };
 
